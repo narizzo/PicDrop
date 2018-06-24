e905f69ce5d9db07172eb5b80a96dc710857471c
Nicholas Rizzo <nrizzo414@gmail.com>
2018-06-24 15:39:29 -0400

    Make initial view controller with main UI

--- PicDrop/PicDrop/ViewController.swift
+++ PicDrop/PicDrop/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var actionBar: UIView!
+  
+  @IBOutlet weak var profile: UIButton!
+  @IBOutlet weak var upvote: UIButton!
+  @IBOutlet weak var downvote: UIButton!
+  @IBOutlet weak var camera: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
-
+  @IBAction func tappedProfile(_ sender: UIButton) {
+    print("profile")
+  }
+  
+  @IBAction func tappedUpvote(_ sender: UIButton) {
+    print("upvote")
+  }
+  
+  @IBAction func tappedDownvote(_ sender: UIButton) {
+    print("downvote")
+  }
+  
+  @IBAction func tappedCamera(_ sender: UIButton) {
+    print("camera")
+    
+  }
+  
 }
-
