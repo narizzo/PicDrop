1b97c1002af111441de26d944b0c29ff602e215c
Nicholas Rizzo <nrizzo414@gmail.com>
2018-06-24 12:47:33 -0400

    Initial Commit

---
+++ PicDrop/PicDrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PicDrop
+//
+//  Created by Nicholas Rizzo on 6/24/18.
+//  Copyright © 2018 Nicholas Rizzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
